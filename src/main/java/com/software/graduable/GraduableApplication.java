c1ed72fdffd73ce3dd938861d503d617f180adfe
KimRPG <120882082+KimRPG@users.noreply.github.com>
2025-04-09 20:17:35 +0900

    :sparkles: test

--- src/main/java/com/software/graduable/GraduableApplication.java
+++ src/main/java/com/software/graduable/GraduableApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GraduableApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GraduableApplication.class, args);
 	}
 
